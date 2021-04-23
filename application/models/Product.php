bb2d9a9156f876809575021e06fe6a5776d07de6
Philip <philip@Philips-MacBook-Pro.local>
2021-04-23 08:11:58 +0800

    Styles fix

--- application/models/Product.php
+++ application/models/Product.php
@@ -1,20 +1,7 @@
 <?php
     class Product extends DataMapper{
-        var $table = 'products';
+        var $table = 'items';
 
-        var $validation = array(
-            "name" => array(
-                "label" => "Name",
-                "rules" => array("required", "trim"),
-            ),
-            "description" => array(
-                "label" => "Description",
-                "rules" => array("required", "trim"),
-            ),
-            "price" => array(
-                "label" => "Price",
-                "rules" => array("required", "numeric"),
-            )
-        );
+        
     }
 ?>
--- application/views/edit.php
+++ application/views/edit.php
@@ -1,27 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url() ?>static/edit.css">
-    <title>PHP | Products</title>
-</head>
-<body>
-    <section>
-        <form action="<?= base_url() ?>update/<?= $get_data->id ?>" method="POST">
-            <input type="hidden" name="<?=$this->security->get_csrf_token_name();?>" value="<?=$this->security->get_csrf_hash();?>" />
-            <h2>Product <?= $get_data->id ?></h2>
-            <label>Name: <input type="text" name="name" id="name" value="<?= $get_data->name ?>"></label>
-<?php if($this->session->userdata("error_name") != NULL){ echo "".$this->session->userdata("error_name").""; $this->session->unset_userdata("error_name");}?>
-            <label>Description: <input type="text" name="description" id="description" value="<?= $get_data->description ?>"></label>
-<?php if($this->session->userdata("error_description") != NULL){ echo "".$this->session->userdata("error_description").""; $this->session->unset_userdata("error_description");}?>
-            <label>Price: <input type="text" name="price" id="price" value="<?= $get_data->price ?>"></label>
-<?php if($this->session->userdata("error_price") != NULL){ echo "".$this->session->userdata("error_price").""; $this->session->unset_userdata("error_price");}?>
-            <input type="submit" class="update" value="Update">
-            <a href="<?= base_url() ?>">Back</a>
-<?php if($this->session->userdata("message") != NULL){ echo "<p class='message'>".$this->session->userdata("message")."</p>"; $this->session->unset_userdata("message");}?>
-        </form>
-    </section>
-</body>
-</html>
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+    $this->load->view("templates/header");
+?>
+<div class="edit">
+    <h3>Product # <?= $get_data->id ?></h3>
+    <form action="/listings/edit/<?=$get_data->id?>" method="post">
+        <input type="hidden" name="<?=$this->security->get_csrf_token_name();?>" value="<?=$this->security->get_csrf_hash();?>" />
+        <input type="hidden" name="id" value="<?=$get_data->id?>">
+        <div>
+            <label for="Manufacturer">Manufacturer</label>
+            <select name="manufacturer" id="manufacturer">
+                <option value="Jansport" <?= $get_data->manufacturer == "Jansport" ? "selected": ""?>>Jansport</option>
+                <option value="Nike" <?= $get_data->manufacturer == "Nike" ? "selected": ""?>>Nike</option>
+                <option value="Adidas" <?= $get_data->manufacturer == "Adidas" ? "selected": ""?>>Adidas</option>
+                <option value="Casio" <?= $get_data->manufacturer == "Casio" ? "selected": ""?>>Casio</option>
+                <option value="Oakley" <?= $get_data->manufacturer == "Oakley" ? "selected": ""?>>Oakley</option>
+            </select>
+            <span class="error"><?= form_error("manufacturer")?></span>
+        </div>
+        <div>
+            <label for="name">Product Name</label>
+            <input type="text" name="name" value="<?= $get_data->product_name?>">
+            <span class="error"><?= form_error("name")?></span>
+        </div>
+        <div>
+            <label for="name">Price ($)</label>
+            <input type="number" name="price" value="<?= $get_data->price?>">
+            <span class="error"><?= form_error("price")?></span>
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description"><?= $get_data->description?></textarea>
+            <span class="error"><?= form_error("description")?></span>
+        </div>
+        <input type="submit" value="Update">
+        <a href="/listings/delete/<?=$get_data->id?>">Delete</a>
+    </form>
+</div>
+
+
+<?php $this->load->view("templates/footer")?>
